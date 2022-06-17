685af68d064309407866a45ec3e6678d0d2022af
Corrado <corrado.lanera@gmail.com>
2022-06-17 10:52:35 +0200

    app can mange multiple strategies

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,13 @@
+
+* Update the application to consider two strategies:WB density and WB
+    hunted.
+* Added a `README.rmd` file as the homepage for the project.
+
 # wbc.counter 0.1.0
+
+* First version of the application.
 
 # wbc.counter 0.0.0.9000
 
-* Setup minimal development tools
+* Setup minimal development tools.
 * Added a `NEWS.md` file to track changes to the package.
--- app/app.R
+++ app/app.R
@@ -14,7 +14,9 @@ library(shinydashboard)
 # Define UI ----
 ui <- fluidPage(
   
-    titlePanel("Field passive ASF surveillance data"),
+#    titlePanel("WBC counter"),
+  titlePanel(title = span("WBC counter",img(src = "wbclogo.png", height = 35))
+),
 
     # use a gradient in background
     setBackgroundColor(
@@ -24,51 +26,71 @@ ui <- fluidPage(
     ),  
     tags$head(tags$style('h3 {color: #2171B5}')),
     tags$head(tags$style('h1 {color: #2171B5}')),
+    tags$head(tags$style('a {color: red}')),
     
-  #  p('paragraph, with some ', strong('bold'), ' text.'),
     p('African swine fever (ASF) is a devastating disease resulting in high mortality in domestic
-    and wild pigs, spreading fast around the world. Implementation based on EFSA opinion 2021 on Exit Strategy.'),
+    and wild pigs, spreading fast around the world. This tool implements two approaches based
+    on EFSA Exit Strategy (2021), aimed to provide robust evidence of the absence of ASF circulation in wild boars (WBs). 
+    One approach uses the WB density to estimate the number of carcasses 
+      and the second estimates this from the number of WBs hunted and tested.'),
   
     fluidRow(
 
     column(9, 
-       column(3, numericInput("wb_dens", 
-                    h3("Wild boar density"),
-                    min=0,
-                    value = 1)),
+           column(3, selectInput("approach", 
+                                 h3(div("Approach"),br()),
+                                 choices = list("", 
+                                                "WB density" = "y", 
+                                                "WB hunted" = "n"),
+                                 selected = "y")),
+           
+           column(3, numericInput("wb_dens", 
+                    #h3(HTML(paste0("Wild boar density (per km",tags$sup("2"),")"))),  
+                    h3(div("Wild boar density"),
+                       div(HTML(paste0("(per km",tags$sup("2"),")")))),  
+                                min = 0,
+                                value = 4)),
       
-             column(3, numericInput("area", 
-                                    h3("Area"), 
-                                    min=0,
-                                    value = 1)),
-             column(3, 
-                    h3("___"),
-                    helpText("Density and Area ...."))
-      )),
+           column(3, numericInput("n_wbhunt2", 
+                    #h3(HTML(paste0("Hunting bag (per 1000 km",tags$sup("2"),")"))),  
+                    h3(div("Hunting bag"),div(HTML(paste0("(per 1000 km",tags$sup("2"),")")))),  
+                  
+                    min = 0, 
+                    value = NA)),
+           column(3,
+                  h3("___"),
+                  helpText("Note: choose the most appropriate approach for your country: WB density if sampling is heterogeneous during the hunting season or WB hunted if robust hunting bag data is available and collaboration with hunters is established."))
+    )),
+       
     
     fluidRow(          
     
       column(9,
         column(3, numericInput("scr", 
                     h3("Screening phase (months)"), 
-                    min=0,
-                    value = 1)),
-        column(3, numericInput("conf_1", 
-                    h3("Confirmatory phase 1 (months)"), 
-                    min=0,
-                    value = 1)),
-        column(3, numericInput("conf_2", 
-                               h3("Confirmatory phase 2 (months)"), 
-                               value = 1)),
+                    min = 0,
+                    value = 9)),
+
+                column(3, numericInput("conf", 
+                    h3("Confirmatory phase (months)"), 
+                    min = 0,
+                    value = 12)),
+
+                column(3, numericInput("area", 
+                  # h3(HTML(paste0("Area (km",tags$sup("2"),")"))),  
+                   h3(div("Area"),div(HTML(paste0("(km",tags$sup("2"),")")))),  
+                   
+                                      min = 0,
+                   value = 5302)),
         
-        column(3,
-                  h3("___"),
-                  helpText("Note: help text"))
+                column(3, 
+                   h3("___"),
+                   helpText("Default input values correspond to WB density approach for Sardinia (Italy) data"))
       )),
-      
+  
     hr(),
     fluidRow(     
-               column(8, offset = 1, uiOutput("exit")), 
+              column(8, offset = 1, uiOutput("exit")), 
               column(8, offset = 1,
                   tags$div(class="header", checked=NA,
 #                  tags$h4("Link to the reference paper:"),
@@ -81,43 +103,53 @@ ui <- fluidPage(
     )
 
 
-
-
 # Define server logic ----
 server <- function(input, output) {
+  
   output$exit=renderUI({
     
-    n_wb = input$wb_dens*1000 #numero cinghiali per 1000km2
-    wb_hrate = 0.9 #tasso di mortalit da caccia 90%
-    wb_drate = 0.1 #tasso di mortalit naturale 10%
-    n_wbh = n_wb*wb_hrate #numero di cinghiali morti per caccia/1000 km2
-    n_dwb = n_wb*wb_drate #numero cinghiali morti per cause naturali/1000 km2
-    p_found = 0.01 #p(trovare le carcasse)=1%
+    if(input$approach == "y") 
+  {
+    n_wb = input$wb_dens*1000 #n. WBs per 1000 km2
+    n_wbhunt1 <- n_wb*0.45 #n. WBs dead during hunt in 1000km2 (45%)
+    wb_natd1 <- ((n_wbhunt1*100)/90)*0.1 #n. WBs dead naturally per 1000km2
+    p_found <- 0.01 #prob. to find carcasses(1%)
+    c_est1 <- wb_natd1*p_found #n. WBC likely to find
     
-    n_found = n_dwb*p_found 
+    #PROPORZIONE SPAZIO/TEMPO
+    #rapporto tempo (Screening 24 months, confirmatory 9 months)
+    ttime <- input$scr+input$conf
+    #rapporto spazio all infected area (5400 km2)
+ 
+    c_est1_space <- (input$area*c_est1)/1000 # n. WBC 
+    c_est1_scr <- (input$scr*c_est1_space)/12 #n. WBC screening
+    c_est1_conf <- (input$conf*(c_est1_space*2))/12 # n. WBC confirm 
+    tc_est1 <- c_est1_scr+c_est1_conf
+  
     
-    n_found_space <- (input$area*n_found)/1000 #proporzione del numero di carcasse per i km2
-    n_found_scr <- (input$scr*n_found_space)/12 #proporzione del numero di carcasse per la fase di screening
-    n_found_conf1 <- (input$conf_1*(n_found_space*2))/12 #proporzione del numero di carcasse per la fase di conferma 9 mesi
-    n_found_conf2 <- (input$conf_2*(n_found_space*2))/12 #proporzione del numero di carcasse per la fase di conferma 14 mesi
-    
-    nwbS_IA <- n_dwb*(input$area/1000) #dead wb in infected area/year
-    n_carcas_IA <- nwbS_IA*wb_drate
-    s1 <- (n_found_scr+n_found_conf1)/n_carcas_IA #35%
-    s2 <- (n_found_scr+n_found_conf2)/n_carcas_IA #43%
-    
-    
-    
-    
-    HTML(paste("&nbsp;&nbsp;&nbsp;&nbsp;<b>N found:</b>", n_found, "<p/>",
-               "&nbsp;&nbsp;&nbsp;&nbsp;<b>N found confirmatory 1:</b> ", round(n_found_conf1,5), "<p/>", 
-               "&nbsp;&nbsp;&nbsp;&nbsp;<b>N found confirmatory 2:</b> ", round(n_found_conf2,5), "<p/>",
-               "&nbsp;&nbsp;&nbsp;&nbsp;<b>Sensitivity of the Surveillance System (s1):</b> ", round(s1,2), "<p/>",
-               "&nbsp;&nbsp;&nbsp;&nbsp;<b>Sensitivity of the Surveillance System (s2):</b> ", round(s2,2), "<p/>",
+    HTML(paste("&nbsp;&nbsp;&nbsp;&nbsp;<b>N. WB carcasses needed in the screening phase:</b> ", round(c_est1_scr), "<p/>", 
+               "&nbsp;&nbsp;&nbsp;&nbsp;<b>N. WB carcasses needed in the confirmatory phase:</b> ", round(c_est1_conf), "<p/>",
                sep = ""))
     
-  }) 
+  } 
+     
+  else {
+    wb_natd2 <- ((input$n_wbhunt2*100)/90)*0.1 #n. WB dead naturally per 1000km2
+    p_found <- 0.01 
+    c_est2 <- wb_natd2*p_found #n. WBC in 1 year
   
+  #PROPORZIONE SPAZIO/TEMPO
+  #rapporto spazio all infected area (5400 km2)
+  c_est2_space <- (input$area*c_est2)/1000 # n. WBC 
+  c_est2_scr <- (input$scr*c_est2_space)/12 # n. WBC screening
+  c_est2_conf <- (input$conf*(c_est2_space*2))/12 # n. WBC confirm 
+  tc_est2 <- c_est2_scr+c_est2_conf
+
+    HTML(paste("&nbsp;&nbsp;&nbsp;&nbsp;<b>N. WB carcasses needed in the screening phase:</b> ", round(c_est2_scr), "<p/>", 
+               "&nbsp;&nbsp;&nbsp;&nbsp;<b>N. WB carcasses needed in the confirmatory phase:</b> ", round(c_est2_conf), "<p/>",
+                sep = ""))
+  }
+      })
   }
 
 # Run the app ----
